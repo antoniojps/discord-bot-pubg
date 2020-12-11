aff264b6cb5a1e16cda5e68957df8a2b7f78f28b
antoniojps <antonio.santos@mindera.com>
2020-12-11 20:56:40 +0000

    refactor: main and remove log

--- src/bot.ts
+++ src/bot.ts
@@ -10,7 +10,6 @@ client.login(process.env.DISCORDJS_BOT_TOKEN)
 
 client.on('ready', () => {
   console.log(`${client?.user?.tag} has logged in.`)
-  console.log(`connect to db`)
 })
 
 client.on("error", (error) => {
