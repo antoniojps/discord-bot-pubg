516b2ffa86fb14d5f79115a5247250d8e56ae230
antoniojps <antonio.santos@mindera.com>
2020-12-22 05:12:51 +0000

    feat: 🎸 add in progress media and title

--- src/embeds/LookingForSomeone.ts
+++ src/embeds/LookingForSomeone.ts
@@ -1,6 +1,15 @@
 import { MessageEmbed } from 'discord.js';
 import { Stats, StatsPartial } from './../services/pubg';
 
+const inProgressMedia = [
+  'https://media2.giphy.com/media/l3mZfUQOvmrjTTkiY/giphy.gif',
+  'https://media.giphy.com/media/3oKIPmaM8aFolCcuI0/giphy.gif',
+  'https://media1.giphy.com/media/3ohs7YomxqOz4GRHcQ/giphy.gif',
+  'https://media1.giphy.com/media/3ohzdOE33hBQ7duPfi/giphy.gif',
+  'https://media.giphy.com/media/qlCFjkSruesco/giphy.gif',
+  'https://media.giphy.com/media/3oKIP5KxPss1gjwpG0/giphy.gif',
+];
+
 const missingPlayersMedia: { [key: number]: string } = {
   1: 'https://i.imgur.com/TvqWGPH.png',
   2: 'https://i.imgur.com/S8jNcjs.png',
@@ -36,15 +45,15 @@ export const NOT_FOUND_MESSAGE = '¯\\_(ツ)_/¯';
 
 export const EmbedLookingForSomeone = ({ author, channel, users, note }: LfsEmbedProps) => {
   const usersList = users?.map((user) => {
-    if (user.pubgNickname === '' || user.stats === undefined) return `\n<@${user.discordId}> ${NOT_FOUND_MESSAGE}`;
+    if (user.pubgNickname === '' || user.stats === undefined) return `\n<@${user.discordId}>${NOT_FOUND_MESSAGE}`;
     return `\n**[${user?.pubgNickname}](https://pubg.op.gg/user/${user?.pubgNickname}?discordId=${user.discordId}&nick=${user?.pubgNickname})** - ${user?.stats?.bestRank}, KD ${user?.stats?.kd}, ADR ${user?.stats?.avgDamage}, WR ${user?.stats?.winRatio}%`;
   });
 
   const missingPlayers = users ? 4 - users.length : 0;
   const missingPlayersContent = users && users.length ? ` +${4 - users.length} ` : ' ';
-  const title = channel
-    ? `Procura${missingPlayersContent}jogadores - ${channel.name}`
-    : `Procura${missingPlayersContent}jogadores`;
+
+  const title = missingPlayers > 0 ? `Procura${missingPlayersContent}jogadores` : `Squad completa`;
+  const titleChannel = channel ? `${title} - ${channel.name}` : title;
   const conclusion = channel
     ? `Para te juntares reaje com ✉️ ou envia PM <@${author.id}>`
     : `Para convidar entra num canal e reaje com 👍 ou envia PM <@${author.id}>`;
@@ -62,9 +71,14 @@ export const EmbedLookingForSomeone = ({ author, channel, users, note }: LfsEmbe
     .setFooter('lfs')
     .setTimestamp();
 
+  const thumbnail =
+    missingPlayers > 0
+      ? missingPlayersMedia[missingPlayers]
+      : inProgressMedia[Math.floor(Math.random() * inProgressMedia.length)];
+
   if (channel) {
-    Embed.setThumbnail(missingPlayersMedia[missingPlayers]).setAuthor(
-      title,
+    Embed.setThumbnail(thumbnail).setAuthor(
+      titleChannel,
       author.avatar
         ? `https://cdn.discordapp.com/avatars/${author.id}/${author.avatar}.png?size=128&channelId=${
             channel.id
